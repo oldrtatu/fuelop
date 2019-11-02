0bc9d6e5aaf6cd77a5ace940e48944a1febb9fd0
riya-mishra <bittuasssd22@gmail.com>
2019-11-02 17:41:22 +0530

    Updates

---
+++ precache-manifest.c5781858d872fff70e0aee4669e4a21e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2125d7905bddc0f1d35385a96a69fa82",
+    "url": "/fuelop/index.html"
+  },
+  {
+    "revision": "474c788ee09c486e72b9",
+    "url": "/fuelop/static/css/main.9ddc353a.chunk.css"
+  },
+  {
+    "revision": "58ce7ab6871ec22aa181",
+    "url": "/fuelop/static/js/2.63a60cb5.chunk.js"
+  },
+  {
+    "revision": "474c788ee09c486e72b9",
+    "url": "/fuelop/static/js/main.980cb918.chunk.js"
+  },
+  {
+    "revision": "9bc7bbab141687f9eeb3",
+    "url": "/fuelop/static/js/runtime-main.864a23be.js"
+  },
+  {
+    "revision": "95228f7bc941b67330a353b0b5cde9c5",
+    "url": "/fuelop/static/media/add.95228f7b.svg"
+  },
+  {
+    "revision": "0b9cd9d92ad7e904bf503cbeabfa0156",
+    "url": "/fuelop/static/media/ar_pr.0b9cd9d9.svg"
+  },
+  {
+    "revision": "ba60e818c0e47d0a362c6dea65d4af75",
+    "url": "/fuelop/static/media/avatar_user.ba60e818.jpg"
+  },
+  {
+    "revision": "184dff3ce654fd7c6d8d10f90d94c590",
+    "url": "/fuelop/static/media/collapse.184dff3c.svg"
+  },
+  {
+    "revision": "aa10a51e925f5b908d8f7ba64ac877fa",
+    "url": "/fuelop/static/media/dashboard.aa10a51e.svg"
+  },
+  {
+    "revision": "8c5fcb0da4b8a3899cc1142779b2c1fd",
+    "url": "/fuelop/static/media/database_maintenance.8c5fcb0d.svg"
+  },
+  {
+    "revision": "1fede408b67ed98280d44f7c59db8d88",
+    "url": "/fuelop/static/media/delete.1fede408.svg"
+  },
+  {
+    "revision": "c8c2d5b5f8d02fea79dbe355cd4f70aa",
+    "url": "/fuelop/static/media/dispatch_board.c8c2d5b5.svg"
+  },
+  {
+    "revision": "1946eb317278f5c5b29d6dbfe62d4d36",
+    "url": "/fuelop/static/media/driver_payroll.1946eb31.svg"
+  },
+  {
+    "revision": "e2074b974d0fb057042646e7b78e56cb",
+    "url": "/fuelop/static/media/ifta.e2074b97.svg"
+  },
+  {
+    "revision": "960a76b4b4e23d388ec31d876f1f4f49",
+    "url": "/fuelop/static/media/logout.960a76b4.svg"
+  },
+  {
+    "revision": "3dfb6bffee683c8c7b981b236a7b0647",
+    "url": "/fuelop/static/media/maintenance.3dfb6bff.svg"
+  },
+  {
+    "revision": "2b413bf3babea72c0b36c40aac441683",
+    "url": "/fuelop/static/media/marker.2b413bf3.svg"
+  },
+  {
+    "revision": "768830b72f1980863207be7af3c7073c",
+    "url": "/fuelop/static/media/marker_second.768830b7.svg"
+  },
+  {
+    "revision": "ccc9981f6546476f466c75fee10cddf3",
+    "url": "/fuelop/static/media/notification.ccc9981f.svg"
+  },
+  {
+    "revision": "db6aea9d2d72be1babaf3080c41708b2",
+    "url": "/fuelop/static/media/order_entry.db6aea9d.svg"
+  },
+  {
+    "revision": "e8497f4a5ede1f051383883ff1089d4f",
+    "url": "/fuelop/static/media/planner.e8497f4a.svg"
+  },
+  {
+    "revision": "df2ad32973a2506ff2215601b0a5d1f5",
+    "url": "/fuelop/static/media/purchase_order.df2ad329.svg"
+  },
+  {
+    "revision": "839b295e867f008bd4efc164ad15e48b",
+    "url": "/fuelop/static/media/quotation.839b295e.svg"
+  },
+  {
+    "revision": "81575bdfb7f6838cbf4c3eeb94e88106",
+    "url": "/fuelop/static/media/search.81575bdf.svg"
+  },
+  {
+    "revision": "4e750a16e1dbdfb0c5f8ca36d8638d6d",
+    "url": "/fuelop/static/media/setting.4e750a16.svg"
+  },
+  {
+    "revision": "0beee21d266a62b331e1bfe09caea17d",
+    "url": "/fuelop/static/media/stops.0beee21d.svg"
+  },
+  {
+    "revision": "a8a359f28cb9b482c2e84521af5777bd",
+    "url": "/fuelop/static/media/taxes.a8a359f2.svg"
+  },
+  {
+    "revision": "623cd46fd8229f24622e81cd4c4c192d",
+    "url": "/fuelop/static/media/vendor_management.623cd46f.svg"
+  }
+]);
